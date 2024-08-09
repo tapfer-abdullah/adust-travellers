7ded488cdef0fa1716e48949d186d402d5b16aae
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-08-10 02:16:43 +0600

    dashboard genaral design done

--- Script/dashboard.js
+++ Script/dashboard.js
@@ -13,11 +13,22 @@ export function decodeAndVerifyJwt(token) {
 }
 
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
+    const allSearchParams = window.location.search.split("?")?.[1]?.split("&");
     const pathname = window.location.pathname;
+    const token = localStorage.getItem("ak-secret");
+
+
+    let searchObj = {};
+    allSearchParams?.forEach(s => {
+        const arr = s.split("=");
+        searchObj[`${arr[0]}`] = arr[1];
+    });
+    console.log({ pathname, searchObj })
+
     if (pathname == "/dashboard/index.html") {
         const existingCart = localStorage.getItem('ak-travelers-cart');
-        const token = localStorage.getItem("ak-secret");
+
         let isLogin = false;
 
         if (token) {
@@ -31,4 +42,45 @@ window.addEventListener("load", () => {
             }
         }
     }
+
+    if (searchObj?.params == "orders") {
+        console.log("orders")
+
+        try {
+            const response = await fetch(`${bashedURL}/order/user`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json(); // Parse the JSON data from the response
+            console.log('Data fetched:', data); // Log the fetched data
+
+
+            if (!data?.success) {
+                return alert("You haven't order anything yet!")
+            }
+
+
+
+        } catch (error) {
+            console.error('Error fetching data:', error.message); // Log any errors
+        }
+    }
+    else if (searchObj?.params == "profile") {
+        console.log("profile")
+    }
+});
+
+
+document.getElementById("log-out").addEventListener("click", () => {
+    localStorage.removeItem("ak-secret");
+    window.location.assign("/index.html");
+    alert("Log out successful!")
 })
